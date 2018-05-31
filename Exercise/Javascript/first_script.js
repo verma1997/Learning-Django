e14667aea62958662bc40693952050dfeb6d8a4d
Priyanshu Verma <201652019@iiitvadodara.ac.in>
2018-05-31 19:41:13 +0530

    Add files via upload
    added

---
+++ Exercise/Javascript/first_script.js
@@ -0,0 +1,6 @@
+alert("Hi ! Let's covert Pounds(lbs) into Kilograms(Kgs)");
+var wt_in_lbs = prompt("Enter the weight in Pounds(lbs): ");
+alert("You've Entered: " + wt_in_lbs + "lbs");
+var wt_in_kg = wt_in_lbs*0.45;
+console.log("Weight in Kilograms(Kgs): " + wt_in_kg);
+alert("Congratulations! Go into the console and see the answer.");
