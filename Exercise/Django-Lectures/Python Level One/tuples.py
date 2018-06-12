196c5f488f87f06d219bc364c43888bf673fcc71
Priyanshu Verma <201652019@iiitvadodara.ac.in>
2018-06-12 14:36:17 +0530

    added tuples.py

---
+++ Exercise/Django-Lectures/Python Level One/tuples.py
@@ -0,0 +1,19 @@
+#Booleans
+True
+False
+
+#Tuples
+myTuple = (1,True,'panda')
+print(myTuple)
+#Tuples are immutable.
+
+#Sets
+mySet = set()
+
+#to add elements in the set
+mySet.add(1)
+mySet.add(3)
+mySet.add(5)
+mySet.add('Priyanshu')
+print(mySet)
+#Note: Set can only add unique elements.
